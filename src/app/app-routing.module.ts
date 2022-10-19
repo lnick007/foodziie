ec9f7e077554edf074c7cbefae2071ee6cecf79d
Anik Duley <Anik.Duley@mjunction.in>
2022-10-19 10:16:51 +0530

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AddCategoryComponent } from './add-category/add-category.component';
+import { AddfoodComponent } from './addfood/addfood.component';
+import { AdminComponent } from './admin/admin.component';
+import { BillComponent } from './bill/bill.component';
+import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
+import { FoodPageComponent } from './components/pages/food-page/food-page.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { LoginCComponent } from './login-c/login-c.component';
+import { LoginMerchantComponent } from './login-merchant/login-merchant.component';
+import { OrderComponent } from './order/order.component';
+import { RegisterComponent } from './register/register.component';
+import { TotalCategoryComponent } from './total-category/total-category.component';
+import { TotalFoodComponent } from './total-food/total-food.component';
+import { TotalOrdersComponent } from './total-orders/total-orders.component';
+import { UsersComponent } from './users/users.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'search/:searchTerm', component: HomeComponent },
+  { path: 'tag/:tag', component: HomeComponent },
+  {path:'food/:id', component:FoodPageComponent},
+  {path:'cart-page', component: CartPageComponent},
+  {path:'loginCustomer',component:LoginCComponent},
+  {path:'loginMerchant',component:LoginMerchantComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'admin',component:AdminComponent},
+  {path:'users',component:UsersComponent},
+  {path:'addfood', component:AddfoodComponent},
+  {path:'add-category',component:AddCategoryComponent},
+  {path:'total-category', component:TotalCategoryComponent},
+  {path:'total-food',component:TotalFoodComponent},
+  {path:'total-order',component:TotalOrdersComponent},
+  {path:'bill', component:BillComponent},
+  {path:'order',component:OrderComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,59 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/partials/header/header.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { RatingModule } from 'ng-starrating';
+import { SearchComponent } from './components/partials/search/search.component';
+import { TagsComponent } from './components/partials/tags/tags.component';
+import { FoodPageComponent } from './components/pages/food-page/food-page.component';
+import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
+import { TitleComponent } from './components/partials/title/title.component';
+import { NotFoundComponent } from './components/partials/not-found/not-found.component';
+import { LoginCComponent } from './login-c/login-c.component';
+import { LoginMerchantComponent } from './login-merchant/login-merchant.component';
+import { RegisterComponent } from './register/register.component';
+import { AdminComponent } from './admin/admin.component';
+import { UsersComponent } from './users/users.component';
+import { AddfoodComponent } from './addfood/addfood.component';
+import { TotalCategoryComponent } from './total-category/total-category.component';
+import { AddCategoryComponent } from './add-category/add-category.component';
+import { TotalFoodComponent } from './total-food/total-food.component';
+import { TotalOrdersComponent } from './total-orders/total-orders.component';
+import { OrderComponent } from './order/order.component';
+import { BillComponent } from './bill/bill.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SearchComponent,
+    TagsComponent,
+    FoodPageComponent,
+    CartPageComponent,
+    TitleComponent,
+    NotFoundComponent,
+    LoginCComponent,
+    LoginMerchantComponent,
+    RegisterComponent,
+    AdminComponent,
+    UsersComponent,
+    AddfoodComponent,
+    TotalCategoryComponent,
+    AddCategoryComponent,
+    TotalFoodComponent,
+    TotalOrdersComponent,
+    OrderComponent,
+    BillComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RatingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
